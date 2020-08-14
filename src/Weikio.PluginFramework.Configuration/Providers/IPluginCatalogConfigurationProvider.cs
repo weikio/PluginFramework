99994c185f1e5f47f9e60a073814652c460f0f7e
Panos Athanasiou <panos_ath@outlook.com>
2020-08-15 02:30:51 +0300

    Add IPluginCatalogConfigurationProvider

---
+++ src/Weikio.PluginFramework.Configuration/Providers/IPluginCatalogConfigurationProvider.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace Weikio.PluginFramework.Configuration.Providers
+{
+    /// <summary>
+    /// Interface that specified the methods a PluginCatalogConfigurationProvider needs.
+    /// </summary>
+    public interface IPluginCatalogConfigurationProvider
+    {
+        /// <summary>
+        /// The key of the section inside the configuration.
+        /// </summary>
+        string SectionKey { get; }
+
+        /// <summary>
+        /// The key of the catalogs section inside the parent configuration section (<see cref="SectionKey"/>).
+        /// </summary>
+        string CatalogsKey { get; }
+
+        /// <summary>
+        /// Returns a list that contains catalog configurations.
+        /// </summary>
+        /// <param name="configuration">The configuration to use.</param>
+        /// <returns>A list that contains catalog configurations.</returns>
+        List<CatalogConfiguration> GetCatalogConfigurations(IConfiguration configuration);
+    }
+}

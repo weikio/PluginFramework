7b696d6dcd9f02c06f6962b1814f949623529d8e
Panos Athanasiou <panos_ath@outlook.com>
2020-08-15 02:29:40 +0300

    Add configuration section base class

---
+++ src/Weikio.PluginFramework.Configuration/CatalogConfiguration.cs
@@ -0,0 +1,13 @@
+namespace Weikio.PluginFramework.Configuration
+{
+    /// <summary>
+    /// Base configuration class for catalogs.
+    /// </summary>
+    public class CatalogConfiguration
+    {
+        /// <summary>
+        /// The type of the catalog this configuration section represents.
+        /// </summary>
+        public string? Type { get; set; }
+    }
+}
